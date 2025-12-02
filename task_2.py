e5e65e8c6b43d2dc65c95a30513f5186af3eb80e
Nataliya Smerdova <smerdova.nataliya@yandex.ru>
2025-12-02 08:13:19 +0300

    Исправлено по результатам ревью

--- task_2.py
+++ task_2.py
@@ -10,7 +10,7 @@ class Comedy(Movies):
         super().__init__()
 
     def add_movie(self, movie):
-        self.movies.append(movie)
+        super().add_movie(movie)
         return f'Комедии: {self.movies}'
     
 class Drama(Movies):
@@ -18,7 +18,7 @@ class Drama(Movies):
         super().__init__()
 
     def add_movie(self, movie):
-        self.movies.append(movie)
+        super().add_movie(movie)
         return f'Драмы: {self.movies}'
 
 comedy = Comedy()
